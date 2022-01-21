b3e53eb91c167376b05b3e10713af19299cdabc5
Jesse Hardage <j.hardage@pm.me>
2022-01-21 17:22:07 +0000

    fix: adding longer expiry to jwt token

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -16,7 +16,7 @@ exports.create = async (req, res) => {
     const hash = await bcrypt.hash(password, 10)
     const newUser = { name, username, email, password: hash }
     const user = await User.add(newUser)
-    const accessToken = jwt.sign(user.id, SECRET_KEY)
+    const accessToken = jwt.sign(user.id, SECRET_KEY, { expiresIn: '5h' })
     res.status(201).send({ accessToken, user })
   } catch (error) {
     res.status(400).send({ error, message: 'Could not create user' })
@@ -29,7 +29,7 @@ exports.login = async (req, res) => {
     const user = await User.getByEmail({ email })
     const validatedPass = await bcrypt.compare(password, user[0].password)
     if (!validatedPass) throw new Error()
-    const accessToken = jwt.sign({ id: user[0].id }, SECRET_KEY)
+    const accessToken = jwt.sign({ id: user[0].id }, SECRET_KEY, { expiresIn: '5h' })
     res.status(200).send({ accessToken, user })
   } catch (error) {
     res
