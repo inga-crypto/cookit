69b0a82b417dc9bd125d0fc40143902db8678624
Hamza <hamza@Hamzas-MacBook-Pro.local>
2022-01-24 15:41:51 +0000

    style: prettier formatting

--- server/router.js
+++ server/router.js
@@ -1,20 +1,20 @@
-'use strict'
+"use strict";
 
-const router = require('express').Router()
-const userController = require('./controllers/users')
-const recipeController = require('./controllers/recipes')
-const authMiddleware = require('./middlewares/auth')
-const multerMiddleware = require('./middlewares/multer')
+const router = require("express").Router();
+const userController = require("./controllers/users");
+const recipeController = require("./controllers/recipes");
+const authMiddleware = require("./middlewares/auth");
+const multerMiddleware = require("./middlewares/multer");
 
 //AUTH
 
-router.post('/api/auth/signup', userController.create)
-router.post('/api/auth/login', userController.login)
+router.post("/api/auth/signup", userController.create);
+router.post("/api/auth/login", userController.login);
 
 //RECIPES
 
-router.get('/api/recipes', recipeController.list)
-router.get('/api/recipes/:id', recipeController.getById)
-router.post('/api/recipes/create', authMiddleware, recipeController.create)
+router.get("/api/recipes", recipeController.list);
+router.get("/api/recipes/:id", recipeController.getById);
+router.post("/api/recipes/create", authMiddleware, recipeController.create);
 
-module.exports = router
+module.exports = router;
